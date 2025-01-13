a0562208ee916aa53cdf073615a4bb2c13cc0d85
Sanjar006 <tomf4858@gmail.com>
2025-01-13 03:53:22 -0600

    DDZ3

--- js/lesson.js
+++ js/lesson.js
@@ -5,40 +5,36 @@ const tabs = document.querySelectorAll(".tab_content_item");
 const tabsParent = document.querySelector(".tab_content_items");
 
 const hideTabContent = () => {
-    tabContentBlocks.forEach((block) => {
-        block.style.display = "none"
-    })
-    tabs.forEach((tab) => {
-        tab.classList.remove('tab_content_item_active')
-    })
-}
+  tabContentBlocks.forEach((block) => {
+    block.style.display = "none";
+  });
+  tabs.forEach((tab) => {
+    tab.classList.remove("tab_content_item_active");
+  });
+};
 
 const showTabContent = (id = 0) => {
-    tabContentBlocks[id].style.display = "block"
-    tabs[id].classList.add('tab_content_item_active')
-}
+  tabContentBlocks[id].style.display = "block";
+  tabs[id].classList.add("tab_content_item_active");
+};
 
-hideTabContent()
-showTabContent()
+hideTabContent();
+showTabContent();
 
-let showIndex = 0
+let showIndex = 0;
 setInterval(() => {
-    hideTabContent();
-    showIndex = (showIndex + 1) % tabs.length;
-    showTabContent(showIndex);
+  hideTabContent();
+  showIndex = (showIndex + 1) % tabs.length;
+  showTabContent(showIndex);
 }, 3000);
 
-tabsParent.onclick = (event) =>{
-    if (event.target.classList.contains('tab_content_item')){
-      tabs.forEach((tab,tabIndex) => {
-          if (event.target === tab){
-              hideTabContent()
-              showTabContent(tabIndex)
-          }
-      })
-    }
-}
-
-
-
-
+tabsParent.onclick = (event) => {
+  if (event.target.classList.contains("tab_content_item")) {
+    tabs.forEach((tab, tabIndex) => {
+      if (event.target === tab) {
+        hideTabContent();
+        showTabContent(tabIndex);
+      }
+    });
+  }
+};
